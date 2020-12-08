41e6d5b5efb9458ffb21a8ee4ba5ba240fb2e26b
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-12-08 14:12:10 -0500

    Add basic testing for cli command

--- test/test_cli.py
+++ test/test_cli.py
@@ -15,21 +15,75 @@
 #
 from click.testing import CliRunner
 from sitkibex.cli import cli
+import os.path
+import pytest
 
 
-def test_cli():
+def _data_files():
+    """
+    A dictionary of test data file names to full paths.
+    """
+
+    data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+
+    file_dic = {}
+    for f in os.listdir(data_dir):
+        full_path = os.path.join(data_dir, f)
+        if os.path.isfile(full_path):
+            file_dic[f] = full_path
+
+    return file_dic
+
+
+data_files = _data_files()
+
+
+def test_cli_help():
     runner = CliRunner()
     result = runner.invoke(cli, ['--help'])
     assert not result.exception
 
 
-def test_cli_reg():
+def test_cli_reg_help():
     runner = CliRunner()
     result = runner.invoke(cli, ['registration', '--help'])
     assert not result.exception
 
 
-def test_cli_resample():
+reg_args = [
+    ['registration', data_files["panel1.nrrd"] + '@JOJO', data_files["panel2.nrrd"] + '@JOJO', 'out.txt'],
+    ['registration', data_files["panel1.nrrd"] + '@4', data_files["panel2.nrrd"] + '@4', 'out.txt'],
+    ['registration', data_files["panel1.nrrd"] + '@Ch5', data_files["panel2.nrrd"] + '@CH5', 'out.txt'],
+]
+
+
+@pytest.mark.parametrize("cli_args", reg_args)
+def test_cli_reg(cli_args):
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, cli_args)
+
+    assert not result.exception
+
+
+resample_args = [
+    "resample {} {} -o test.nrrd".format(data_files["panel1.nrrd"], data_files["panel2.nrrd"]),
+    "resample {} {}@Ch1 -o test.nrrd".format(data_files["panel1.nrrd"], data_files["panel2.nrrd"]),
+    "resample --fusion {}@JOJO {}@JOJO -o test.nrrd".format(data_files["panel1.nrrd"], data_files["panel2.nrrd"]),
+    "resample  --bin 2 --fusion --projection {}@1 {}@Ch1 -o test.png".format(data_files["panel1.nrrd"],
+                                                                             data_files["panel2.nrrd"])
+]
+
+
+@pytest.mark.parametrize("cli_args", resample_args)
+def test_cli_resample(cli_args):
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, cli_args.split())
+    assert not result.exception
+
+
+def test_cli_resample_help():
     runner = CliRunner()
     result = runner.invoke(cli, ['resample', '--help'])
     assert not result.exception
