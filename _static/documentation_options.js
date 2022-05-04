558f85ce5cd1ae47a0468b67f33ea1072fc4d3be
blowekamp <blowekamp@bots.github.com>
2022-05-04 19:20:39 +0000

    Automatic Update
    Wed May  4 19:20:39 UTC 2022

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.dev4',
+    VERSION: '0.4.dev5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
